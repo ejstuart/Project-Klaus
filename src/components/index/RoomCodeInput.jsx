6a4f0794ea539aad98b3f4c81451f78b3fb96aaa
Eric Stuart <estu836@aucklanduni.ac.nz>
2020-04-14 13:17:40 +1200

    Refactored RoomCodeInput into seperate component.

---
+++ src/components/index/RoomCodeInput.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {Input, InputGroupAddon, InputGroupText, InputGroup} from 'reactstrap';
+
+export default class RoomCodeInput extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {focused: false};
+
+        this.onRoomCodeFocus = this.onRoomCodeFocus.bind(this);
+        this.onRoomCodeBlur = this.onRoomCodeBlur.bind(this);
+    }
+
+    onRoomCodeFocus(){
+        this.setState({
+            focused: true
+        });
+    }
+
+    onRoomCodeBlur(){
+        this.setState({
+            focused: false
+        });
+    }
+
+    render() {
+        return (
+            <InputGroup size="lg" className={this.state.focused ? "input-group-focus":""}>
+            <InputGroupAddon addonType="prepend">
+                <InputGroupText>
+                    <i className="tim-icons icon-app" />
+                </InputGroupText>
+            </InputGroupAddon>
+            <Input className="mb-2 text-center text-uppercase"
+                   type="text"
+                   placeholder="Room Code"
+                   maxLength={6}
+                   bsSize="lg"
+                   value={this.props.code}
+                   onChange={this.props.onChange}
+                   onFocus={this.onRoomCodeFocus}
+                   onBlur={this.onRoomCodeBlur}
+            />
+        </InputGroup>
+        );
+    }
+}
+
