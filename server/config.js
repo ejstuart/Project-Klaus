6792b30bb325f7cc6e47b8d340a5088e5cfc1ded
Jackson Chadfield <jackson.chadfield@jacksonc.dev>
2020-10-06 13:43:36 +1300

    remove server folder

--- server/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    DB: process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://localhost:27017/klaus',
-    SECRET: process.env.SECRET_KEY ? process.env.SECRET_KEY : "changeme"
-}
-
-export default config;
